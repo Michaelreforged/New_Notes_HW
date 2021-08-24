391ed6b32e8be4b009d2e19efb550b8cc2df0d71
Michael Cheung <michaelcheung.cheme@gmail.com>
2021-08-24 08:50:31 -0600

    Delete Done added some comments in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "notes#index"
-  get "/notes", to: "notes#index"
-  get "/notes/new", to: "notes#new"
-  post "/notes", to: "notes#create"
-  get "/notes/:id", to: "notes#show"
-  get "/notes/:id/edit", to: "notes#edit"
 
-  patch "/notes/:id", to: "notes#update"
-  put "/notes/:id", to: "notes#update"
-  delete "/notes/:id", to: "notes#destroy"
+  get "/notes", to: "notes#index"          #View All
+
+  get "/notes/new", to: "notes#new"        # View new page w/ form to add new notes
+
+  post "/notes", to: "notes#create"        # Creates new notes
+  get "/notes/:id", to: "notes#show"       # Show Individual notes
+  get "/notes/:id/edit", to: "notes#edit"  # Edit new note
+
+  patch "/notes/:id", to: "notes#update"   # Updates notes
+  put "/notes/:id", to: "notes#update"     #
+  delete "/notes/:id", to: "notes#destroy" # Deletes Notes
 
 end
